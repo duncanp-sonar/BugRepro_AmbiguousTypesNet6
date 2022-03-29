71057a1e7bb44b813ef95aa9f6a3a2e272a2a96e
duncanp-sonar <duncan.pocklington@sonarsource.com>
2022-03-29 16:10:50 +0100

    Remove unnecessary batch files

--- coreSCAnalyze.bat
+++
@@ -1,7 +0,0 @@
-cls
-
-dotnet D:\tools\scanner\5.5.3\net5.0\SonarScanner.MSBuild.dll begin /k:dp_incidents_ambig1 /d:sonar.login=%SC% /o:duncanp-sonar-test /d:sonar.host.url=https://sonarcloud.io
-
-dotnet build -t:rebuild -bl -p:configuration=release -v:normal
-
-::dotnet D:\tools\scanner\5.5.3\net5.0\SonarScanner.MSBuild.dll end /d:sonar.login=%SC%
--- netfxSCAnalyze.bat
+++
@@ -1,7 +0,0 @@
-cls
-
-D:\tools\scanner\5.5.3\net46\SonarScanner.MSBuild.exe begin /k:dp_incidents_ambig1 /d:sonar.login=%SC% /o:duncanp-sonar-test /d:sonar.host.url=https://sonarcloud.io
-
-msbuild.exe -t:rebuild -bl -p:configuration=release -v:normal -nr:false
-
-::D:\tools\scanner\5.5.3\net46\SonarScanner.MSBuild.exe end /d:sonar.login=%SC%
